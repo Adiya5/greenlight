47dac144077eb0de3e354e42e745bec85a2eda21
Adiya <adiya.sakhipkereyeva@gmail.com>
2022-12-26 13:28:23 +0600

    first commit

---
+++ internal/data/movies.go
@@ -0,0 +1,115 @@
+package data
+
+import (
+	"database/sql"
+	"github.com/shynggys9219/greenlight/internal/validator"
+	"time"
+
+	"github.com/lib/pq"
+)
+
+// By default, the keys in the JSON object are equal to the field names in the struct ( ID,
+// CreatedAt, Title and so on).
+type Movie struct {
+	ID        int64     `json:"id"`                       // Unique integer ID for the movie
+	CreatedAt time.Time `json:"-"`                        // Timestamp for when the movie is added to our database, "-" directive, hidden in response
+	Title     string    `json:"title"`                    // Movie title
+	Year      int32     `json:"year,omitempty"`           // Movie release year, "omitempty" - hide from response if empty
+	Runtime   int32     `json:"runtime,omitempty,string"` // Movie runtime (in minutes), "string" - convert int to string
+	Genres    []string  `json:"genres,omitempty"`         // Slice of genres for the movie (romance, comedy, etc.)
+	Version   int32     `json:"version"`                  // The version number starts at 1 and will be incremented each
+	// time the movie information is updated
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+// Define a MovieModel struct type which wraps a sql.DB connection pool.
+type MovieModel struct {
+	DB *sql.DB
+}
+
+// method for inserting a new record in the movies table.
+func (m MovieModel) Insert(movie *Movie) error {
+	query := `
+		INSERT INTO movies(title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version`
+
+	return m.DB.QueryRow(query, &movie.Title, &movie.Year, &movie.Runtime, pq.Array(&movie.Genres)).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+}
+
+// method for fetching a specific record from the movies table.
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	query := `
+SELECT id, created_at, title, year, runtime, genres, version
+FROM movies
+WHERE id = $1`
+	var movie Movie
+	m.DB.QueryRow(query, id).Scan(&movie.ID, &movie.CreatedAt, &movie.Title,
+		&movie.Year,
+		&movie.Runtime,
+		pq.Array(&movie.Genres),
+		&movie.Version)
+	return &movie, m.DB.QueryRow(query, id).Err()
+}
+
+// method for updating a specific record in the movies table.
+func (m MovieModel) Update(movie *Movie) error {
+	query := `
+	UPDATE movies
+	SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+	WHERE id = $5
+	RETURNING version`
+
+	return m.DB.QueryRow(query, movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID).Scan(
+		&movie.Version)
+	// Use the QueryRow() method to execute the query, passing in the args slice as a
+	// variadic parameter and scanning the new version value into the movie struct.
+
+}
+
+// method for deleting a specific record from the movies table.
+func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+	// Construct the SQL query to delete the record.
+	query := `
+DELETE FROM movies
+WHERE id = $1`
+
+	result, err := m.DB.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	// Call the RowsAffected() method on the sql.Result object to get the number of rows
+	// affected by the query.
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	// If no rows were affected, we know that the movies table didn't contain a record
+	// with the provided ID at the moment we tried to delete it. In that case we
+	// return an ErrRecordNotFound error.
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+	return nil
+
+}
